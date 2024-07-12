65ca9330a361728b0ad8267a331888a58ab79826
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-07-12 18:29:13 +0300

    created the section2 component and added svg and png files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Section1 from "./components/Section1/Section1";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Section1 />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,38 +13,39 @@ const Header = () => {
         setMenuOpen(false);
       }
     };
+
     window.addEventListener('resize', handleResize);
     handleResize();
+
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return (
-    <header className={`${styles.header}`}>
-      <div className={`${styles.headerInner}`}>
-        <div className={`${styles.title}`}>
-          Collers
-        </div>
-        <div className="hidden md:flex space-x-4">
-          <a href="#" className={`${styles.menuLink}`}>Products</a>
-          <a href="#" className={`${styles.menuLink}`}>Solutions</a>
-          <a href="#" className={`${styles.menuLink}`}>Pricing</a>
-          <a href="#" className={`${styles.menuLink}`}>Resources</a>
-          <a href="#" className={`${styles.menuLink}`}>Log In</a>
-          <button className={`${styles.button} `}>Sign up Now</button>
-        </div>
+    <header className={styles.header}>
+      <div className={styles.headerInner}>
+        <div className={styles.title}>Collers</div>
+        {menuOpen ? (
+          <div className={styles.mobileMenu}>
+            <a href="#" className={styles.menuLink}>Products</a>
+            <a href="#" className={styles.menuLink}>Solutions</a>
+            <a href="#" className={styles.menuLink}>Pricing</a>
+            <a href="#" className={styles.menuLink}>Resources</a>
+            <a href="#" className={styles.menuLink}>Log In</a>
+            <button className={styles.menuScale}>
+              <img src={MenuIcon} alt="Menu Icon" className="w-6 h-6" />
+            </button>
+          </div>
+        ) : (
+          <div className="hidden md:flex space-x-4">
+            <a href="#" className={styles.menuLink}>Products</a>
+            <a href="#" className={styles.menuLink}>Solutions</a>
+            <a href="#" className={styles.menuLink}>Pricing</a>
+            <a href="#" className={styles.menuLink}>Resources</a>
+            <a href="#" className={styles.menuLink}>Log In</a>
+            <button className={styles.button}>Sign up Now</button>
+          </div>
+        )}
       </div>
-      {menuOpen && (
-        <div className={`${styles.mobileMenu}`}>
-          <a href="#" className={`${styles.menuLink}`}>Products</a>
-          <a href="#" className={`${styles.menuLink}`}>Solutions</a>
-          <a href="#" className={`${styles.menuLink}`}>Pricing</a>
-          <a href="#" className={`${styles.menuLink}`}>Resources</a>
-          <a href="#" className={`${styles.menuLink}`}>Log In</a>
-          <button className={`${styles.menuScale}`}>
-            <img src={MenuIcon} alt="Menu Icon" className="w-6 h-6" />
-          </button>
-        </div>
-      )}
     </header>
   );
 };
