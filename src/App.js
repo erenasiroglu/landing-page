df1d71a13d6adbc3110bffa9033c3d747ebe1110
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-07-12 22:12:54 +0300

    created the section3 components and added svg icons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Section1 from "./components/Section1/Section1";
 import Section2 from "./components/Section2/Section2";
+import Section3 from "./components/Section3/Section3";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Section1 />
       <Section2 />
+      <Section3 />
     </div>
   );
 }
---
+++ src/components/Section3/Section3.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./Section3.module.css";
+import sectionSvg from "../../assets/images/Deco-video.svg";
+import videoSvg from "../../assets/images/Video.svg";
+import checkSvg from "../../assets/icons/check.svg";
+
+const Section3 = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.containerInner}>
+                <div className={styles.content}>
+                    <div className={styles.title}>Why join us</div>
+                    <div className={styles.subtitle}>
+                        <div className={styles.text}>
+                            <img src={checkSvg} className={styles.icon} alt="Check icon" />
+                            Est et in pharetra magna adipiscing ornare aliquam.
+                        </div>
+                        <div className={styles.text}>
+                            <img src={checkSvg} className={styles.icon} alt="Check icon" />
+                            Tellus arcu sed consequat ac velit ut eu blandit.
+                        </div>
+                        <div className={styles.text}>
+                            <img src={checkSvg} className={styles.icon} alt="Check icon" />
+                            Ullamcorper ornare in et egestas dolor orci.
+                        </div>
+                        <div className={styles.button}>Sign up now</div>
+                    </div>
+                </div>
+                <img src={sectionSvg} className={styles.sectionSvg} alt="Section SVG" />
+                <img src={videoSvg} className={styles.videoSvg} alt="Video SVG" />
+            </div>
+        </div>
+    );
+}
+
+export default Section3;
