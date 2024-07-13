c05e577cd124e04d703f7b90fa1af6f5a7fc1e8f
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-07-13 11:59:30 +0300

    created the section6 component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Section2 from "./components/Section2/Section2";
 import Section3 from "./components/Section3/Section3";
 import Section4 from "./components/Section4/Section4";
 import Section5 from "./components/Section5/Section5";
+import Section6 from "./components/Section6/Section6";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Section3 />
       <Section4 />
       <Section5 />
+      <Section6 />
     </div>
   );
 }
---
+++ src/components/Section6/Section6.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./Section6.module.css";
+import shieldSvg from "../../assets/images/chart.svg";
+import collectionSvg from "../../assets/images/Ellipses.svg";
+import tooltipSvg from "../../assets/images/tooltip.svg";
+
+const Section6 = () => {
+  return (
+    <div className={styles.section}>
+      <div className={styles.background}>
+        <img src={shieldSvg} alt="Shield" className={styles.shield} />
+        <img
+          src={collectionSvg}
+          alt="Collection"
+          className={styles.collection}
+        />
+        <img
+          src={tooltipSvg}
+          alt="Tooltip"
+          className={styles.tooltip}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.title}>11,658,467</div>
+        <div className={styles.subtitle}>Shoes Collected</div>
+      </div>
+    </div>
+  );
+};
+
+export default Section6;
